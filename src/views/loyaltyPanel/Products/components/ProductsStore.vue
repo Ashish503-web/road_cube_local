b649a944e1ef88935bf2284e1c10eb81d8be9ba5
razmikdev11 <razmikbortorian188@gmail.com>
2020-09-24 12:09:48 +0400

    rady tab product store and product series

--- src/views/loyaltyPanel/Products/components/ProductsStore.vue
+++ src/views/loyaltyPanel/Products/components/ProductsStore.vue
@@ -1,76 +1,225 @@
 <template>
-    <div>
-        <v-simple-table
-                :dense="dense"
-                :fixed-header="fixedHeader"
-                :light="light"
+    <v-card>
+        <v-card-title>
+            <v-col class="d-flex" cols="12" sm="2">
+                <v-select
+                        :items="pageItems"
+                        outlined
+                        v-model="pageItem"
+                ></v-select>
+            </v-col>
+            <v-spacer></v-spacer>
+            <v-text-field
+                    v-model="search"
+                    append-icon="mdi-magnify"
+                    placeholder="Search"
+                    outlined
+            ></v-text-field>
+        </v-card-title>
+        <v-data-table
+                :headers="headers"
+                :items="desserts"
+                :items-per-page="5"
+                class="elevation-1"
         >
-            <template v-slot:default>
-                <thead>
+            <template v-slot:item="row">
                 <tr>
-                    <th class="text-left">Name</th>
-                    <th class="text-left">Calories</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="item in desserts" :key="item.name">
-                    <td>{{ item.name }}</td>
-                    <td>{{ item.calories }}</td>
-                </tr>
-                </tbody>
-            </template>
-        </v-simple-table>
+                    <td>{{ row.item.name }}</td>
+                    <td>
+                        <v-checkbox v-model="row.item.market1"></v-checkbox>
+                    </td>
+                    <td>
+                        <v-checkbox v-model="row.item.market2"></v-checkbox>
+                    </td>
+                    <td>
+                        <v-checkbox v-model="row.item.market3"></v-checkbox>
+                    </td>
+                    <td>
+                        <v-checkbox v-model="row.item.market4"></v-checkbox>
+                    </td>
+                    <td>
+                        <v-checkbox v-model="row.item.market5"></v-checkbox>
+                    </td>
+                    <td>
+                        <v-checkbox v-model="row.item.market6"></v-checkbox>
+                    </td>
+                    <td>
+                        <v-checkbox v-model="row.item.market7"></v-checkbox>
+                    </td>
+                    <td>
+                        <v-checkbox v-model="row.item.market8"></v-checkbox>
+                    </td>
+                    <td>
+                        <v-checkbox v-model="row.item.market9"></v-checkbox>
+                    </td>
+                    <td>
+                        <v-checkbox v-model="row.item.market10"></v-checkbox>
+                    </td>
+                    <td>
+                        <v-checkbox v-model="row.item.market11"></v-checkbox>
+                    </td>
+                    <td>
+                        <v-checkbox v-model="row.item.market12"></v-checkbox>
+                    </td>
 
-    </div>
+                </tr>
+            </template>
+        </v-data-table>
+
+    </v-card>
 </template>
 <script>
+    import {
+        mdiMagnify,
+    } from "@mdi/js";
+
     export default {
         data: () => ({
             dense: false,
-            fixedHeader: false,
-            light:true,
+            fixedHeader: true,
+            light: true,
+            search: '',
+            pageItems: [
+                10,
+                20,
+                30,
+                'All'
+            ],
+            pageItem: 10,
+            icons: {
+                mdiMagnify,
+
+            },
+            headers: [
+                {text: 'Name'},
+                {text: 'market1'},
+                {text: 'market2'},
+                {text: 'market3'},
+                {text: 'market4'},
+                {text: 'market5'},
+                {text: 'market6'},
+                {text: 'market7'},
+                {text: 'market8'},
+                {text: 'market9'},
+                {text: 'market10'},
+                {text: 'market11'},
+                {text: 'market12'},
+            ],
             desserts: [
                 {
+                    id: 1,
                     name: 'Frozen Yogurt',
-                    calories: 159,
+                    market1: true,
+                    market2: 0,
+                    market3: 1,
+                    market4: 1,
+                    market5: 1,
+                    market6: 0,
+                    market7: 0,
+                    market8: 0,
+                    market9: 1,
+                    market10: 1,
+                    market11: 0,
+                    market12: 1,
                 },
                 {
-                    name: 'Ice cream sandwich',
-                    calories: 237,
+                    id: 2,
+                    name: 'Arthoures',
+                    market1: true,
+                    market2: 1,
+                    market3: 0,
+                    market4: 0,
+                    market5: 0,
+                    market6: 1,
+                    market7: 1,
+                    market8: 1,
+                    market9: 0,
+                    market10: 0,
+                    market11: 1,
+                    market12: 0,
                 },
                 {
-                    name: 'Eclair',
-                    calories: 262,
+                    id: 3,
+                    name: 'Product 3',
+                    market1: true,
+                    market2: 1,
+                    market3: 0,
+                    market4: 0,
+                    market5: 0,
+                    market6: 1,
+                    market7: 1,
+                    market8: 1,
+                    market9: 0,
+                    market10: 0,
+                    market11: 1,
+                    market12: 0,
                 },
                 {
-                    name: 'Cupcake',
-                    calories: 305,
+                    id: 4,
+                    name: 'Product 4',
+                    market1: true,
+                    market2: 0,
+                    market3: 1,
+                    market4: 1,
+                    market5: 0,
+                    market6: 1,
+                    market7: 0,
+                    market8: 1,
+                    market9: 0,
+                    market10: 0,
+                    market11: 1,
+                    market12: 0,
                 },
                 {
-                    name: 'Gingerbread',
-                    calories: 356,
+                    id: 5,
+                    name: 'Product 5',
+                    market1: true,
+                    market2: 0,
+                    market3: 0,
+                    market4: 0,
+                    market5: 0,
+                    market6: 0,
+                    market7: 1,
+                    market8: 1,
+                    market9: 1,
+                    market10: 1,
+                    market11: 1,
+                    market12: 1
                 },
                 {
-                    name: 'Jelly bean',
-                    calories: 375,
+                    id: 6,
+                    name: 'Product 6',
+                    market1: true,
+                    market2: 0,
+                    market3: 0,
+                    market4: 0,
+                    market5: 0,
+                    market6: 0,
+                    market7: 1,
+                    market8: 1,
+                    market9: 1,
+                    market10: 1,
+                    market11: 1,
+                    market12: 1
+                }, {
+                    id: 7,
+                    name: 'Product 7',
+                    market1: true,
+                    market2: 1,
+                    market3: 1,
+                    market4: 1,
+                    market5: 1,
+                    market6: 1,
+                    market7: 1,
+                    market8: 1,
+                    market9: 1,
+                    market10: 1,
+                    market11: 1,
+                    market12: 1
                 },
-                {
-                    name: 'Lollipop',
-                    calories: 392,
-                },
-                {
-                    name: 'Honeycomb',
-                    calories: 408,
-                },
-                {
-                    name: 'Donut',
-                    calories: 452,
-                },
-                {
-                    name: 'KitKat',
-                    calories: 518,
-                },
+
             ],
         }),
+        methods: {},
     }
 </script>
--- src/views/loyaltyPanel/Products/components/SeriesProducts.vue
+++ src/views/loyaltyPanel/Products/components/SeriesProducts.vue
@@ -1,47 +1,66 @@
 <template>
-    <v-card
-            class="mx-auto"
-            max-width="500"
-    >
-        <v-list>
-            <v-list-item-group v-model="model">
-                <v-list-item
-                        v-for="(item, i) in items"
-                        :key="i"
-                >
-                    <v-list-item-icon>
-                        <v-icon v-text="item.icon"></v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title v-text="item.text"></v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list-item-group>
-        </v-list>
-    </v-card>
+    <div >
+        <h2  class="text-center mt-5 mb-5"
+             >Change the order</h2>
+        <v-card
+                class="mx-auto mt-5 mb-5"
+                max-width="800"
+
+        >
+            <div class="mb-5 pt-5 text-right">
+                <v-btn class="mr-5" color="primary" depressed>Update</v-btn>
+            </div>
+            <v-list class="pr-3 pl-3" max-width="70%">
+                <v-list-item-group>
+
+                    <v-list-item
+                            class="grey lighten-3 mb-1"
+                            v-for="(item, i) in items"
+                            :key="i"
+                    >
+                        <v-list-item-icon>
+                            <v-icon v-text="icons.mdiSwapVerticalBold"></v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title v-text="item.text"></v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+            <div class="mb-5 pb-5 text-right">
+                <v-btn class="mr-5" color="primary" depressed>Update</v-btn>
+            </div>
+        </v-card>
+
+    </div>
 </template>
 <script>
+    import {
+        mdiSwapVerticalBold,
+    } from "@mdi/js";
+
     export default {
         data: () => ({
+            icons: {
+                mdiSwapVerticalBold,
+
+            },
             items: [
                 {
-                    icon: 'mdi-inbox',
-                    text: 'Inbox',
+                    text: 'dd444',
                 },
                 {
-                    icon: 'mdi-star',
-                    text: 'Star',
+                    text: 'kst2',
                 },
                 {
                     icon: 'mdi-send',
                     text: 'Send',
                 },
                 {
-                    icon: 'mdi-email-open',
                     text: 'Drafts',
                 },
             ],
-            model: 1,
+
         }),
     }
 </script>
