41f3748a053e0c30de425e1da596a39acacf4a37
Karen <tarasbabelov@gmail.com>
2020-10-21 14:47:03 +0400

    Home UI

---
+++ src/components/home/profucts/tabs/LoyaltyWithoutBuilding.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="loyaltyWithoutBuildings">
+        <v-container>
+            <v-row class="d-flex align-center justify-center py-0 px-2 px-md-4">
+                <v-col cols="12" md="7" class="py-0 px-2 px-md-4">
+                    <v-img src="../../../../assets/home/img-healthins.png" max-width="530px" width="100%" height="auto"></v-img>
+                </v-col>
+                <v-col cols="12" md="5" class="py-0 px-2 px-md-4">
+                    <v-img
+                            src="../../../../assets/home/icon-heart.png"
+                            max-width="60px"
+                            width="100%"
+                            height="auto"
+                            class="mb-6"
+                    ></v-img>
+                    <h2 class="mb-6">Loyalty Without Building</h2>
+                    <p class="mb-12">Customize your loyalty platform without  the development costs and delays. We make your custom loyalty scheme with our ready made modules and you just cover your monthly subscription. </p>
+                    <v-btn
+                            class="text-capitalize pr-1 pl-0 btn-hover"
+                            height="auto"
+                            rounded
+                    >
+                        <p class="ma-0" style="padding: 15px 70px 15px 20px; font-size: 17px; line-height: normal; text-transform: none">Get a quote</p>
+                        <v-avatar size="38" class="icon-hover">
+                            <v-icon
+                                    size="24"
+                                    color="#fff"
+                                    v-text="icons.mdiArrowRight"
+                                    class="right-icon"
+                            ></v-icon>
+                            <v-icon
+                                    size="24"
+                                    color="#001266"
+                                    v-text="icons.mdiArrowRight"
+                                    class="right-icon"
+                            ></v-icon>
+
+                        </v-avatar>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    import { mdiArrowRight } from "@mdi/js";
+
+    export default {
+        name: "LoyaltyWithoutBuilding",
+        data: () => ({
+            icons: {
+                mdiArrowRight,
+            }
+        })
+    };
+</script>
+
+<style scoped>
+    .loyaltyWithoutBuildings{
+        padding-top: 100px;
+    }
+    h2{
+        color: #142752;
+        font-size: 2.176em;
+        line-height: 1.3em;
+    }
+    p{
+        font-size: 17px;
+        line-height: 1.9em;
+    }
+    /*Hovered btn*/
+    .btn-hover {
+        font-size: 1em;
+        background-color: white !important;
+        border-radius: 150px;
+        transition: all .3s ease;
+        overflow: hidden;
+        box-shadow: none;
+        border: 1px solid #e8eaee;
+    }
+
+    .btn-hover:hover {
+        border: 1px solid #142752;
+        background-color: #142752 !important;
+    }
+
+    .icon-hover {
+        position: absolute;
+        right: 0;
+        display: flex;
+        width: 40px !important;
+        height: 40px !important;
+        background-color: #c8f2fd !important;
+        transition: all .3s ease;
+        justify-items: center;
+        justify-content: flex-end;
+    }
+
+    .icon-hover .right-icon {
+        width: 40px !important;
+    }
+
+    .btn-hover:hover .icon-hover .right-icon {
+        transform: translateX(100%);
+    }
+
+    .btn-hover:hover .icon-hover {
+        background-color: #0089f3 !important;
+    }
+
+    .btn-hover:hover p {
+        color: white;
+    }
+
+    /*Hovered btn*/
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,68 @@
 <template>
-    <v-container fluid>
-        <v-row justify="center">
-            <v-col cols="4">
-                <v-btn color="primary" outlined block to="/loyaltyPanel"
-                    >go to loyalty panel</v-btn
-                >
-                <v-btn
-                    color="primary"
-                    class="mt-3"
-                    outlined
-                    block
-                    to="/storePanel"
-                    >go to store panel</v-btn
-                >
-                <v-btn
-                    id="features"
-                    color="primary"
-                    class="mt-3"
-                    outlined
-                    block
-                    to="/sign-in"
-                >
-                    sign in
-                    <v-icon class="ml-1" v-text="icons.mdiLogin"></v-icon>
-                </v-btn>
-            </v-col>
-        </v-row>
+    <v-container fluid class="pa-0 homePage">
 
+        <HomeSection />
+        <Quickfeatures />
+        <Products />
+        <About />
+        <Cta />
+        <Features />
+        <Quote />
         <SimplePricing />
+        <Footer />
+
+<!--        <v-row justify="center">-->
+<!--            <v-col cols="4">-->
+<!--                <v-btn color="primary" outlined block to="/loyaltyPanel"-->
+<!--                >go to loyalty panel</v-btn-->
+<!--                >-->
+<!--                <v-btn-->
+<!--                        color="primary"-->
+<!--                        class="mt-3"-->
+<!--                        outlined-->
+<!--                        block-->
+<!--                        to="/storePanel"-->
+<!--                >go to store panel</v-btn-->
+<!--                >-->
+<!--                <v-btn-->
+<!--                        id="features"-->
+<!--                        color="primary"-->
+<!--                        class="mt-3"-->
+<!--                        outlined-->
+<!--                        block-->
+<!--                        to="/sign-in"-->
+<!--                >-->
+<!--                    sign in-->
+<!--                    <v-icon class="ml-1" v-text="icons.mdiLogin"></v-icon>-->
+<!--                </v-btn>-->
+<!--            </v-col>-->
+<!--        </v-row>-->
     </v-container>
 </template>
 
 <script>
 import { mdiLogin } from "@mdi/js";
-import SimplePricing from "@/components/home/SimplePricing.vue";
+import HomeSection from "../components/home/HomeSection";
+import Quickfeatures from "../components/home/Quickfeatures";
+import Products from "../components/home/profucts/Products";
+import About from "../components/home/About";
+import Cta from "../components/home/Cta";
+import Features from "../components/home/Features";
+import Quote from "../components/home/Quote";
+import SimplePricing from "../components/home/SimplePricing";
+import Footer from "../components/home/Footer";
 
 export default {
     name: "Home",
-    components: { SimplePricing },
+    components: { Footer, SimplePricing, Quote, Features, Cta, About, Products, Quickfeatures, HomeSection },
     data: () => ({
         icons: { mdiLogin }
     })
 };
+
 </script>
+<style scoped>
+    .homePage{
+        scroll-behavior: smooth;
+    }
+</style>
