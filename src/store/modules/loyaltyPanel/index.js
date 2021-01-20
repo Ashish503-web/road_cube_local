88134f5f9d0e8ab6bfa83e946a225c849f2dc6bf
edgarartsrunyan <edgarartsrunyan552@gmail.com>
2021-01-20 14:14:53 -0800

    small change

--- src/store/modules/loyaltyPanel/index.js
+++ src/store/modules/loyaltyPanel/index.js
@@ -22,7 +22,8 @@ export default {
         businessProfile,
         catalogManagement,
         branchDebt,
-        productSeries
+        productSeries,
+        productsPerStore
     },
 
     state: () => ({
